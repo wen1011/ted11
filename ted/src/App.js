e851b0a00926e5e04e2cf5782d122f01b7ff666c
Chiang Wen-Yun <jennychiang2016@wj-ar.com>
2022-08-19 18:41:07 +0800

    frist

--- ted/src/App.js
+++ ted/src/App.js
@@ -3,30 +3,11 @@ import './App.css'
 function App() {
     return (
         <div>
-            <span className="nes-text is-primary">Primary 哈哈哈</span>
-            <span className="nes-text is-success">Success</span>
-            <span className="nes-text is-warning">Warning</span>
-            <span className="nes-text is-error">Error</span>
-            <span className="nes-text is-disabled">Disabled</span>
-            <a class="nes-btn" href="#">
-                Normal
-            </a>
-
-            <button type="button" className="nes-btn is-primary">
-                Primary
-            </button>
-            <button type="button" className="nes-btn is-success">
-                Success
-            </button>
-            <button type="button" className="nes-btn is-warning">
-                Warning
-            </button>
-            <button type="button" className="nes-btn is-error">
-                Error
-            </button>
-            <button type="button" className="nes-btn is-disabled">
-                Disabled
-            </button>
+            <span className="nes-text is-primary">我們</span>
+            <span className="nes-text is-success">要在</span>
+            <span className="nes-text is-warning">哪裡</span>
+            <span className="nes-text is-error">集合</span>
+            <span className="nes-text is-disabled">?</span>
             <br></br>
             <label>
                 <input type="radio" className="nes-radio" name="answer" checked />
@@ -37,16 +18,13 @@ function App() {
                 <input type="radio" className="nes-radio" name="answer" />
                 <span>No</span>
             </label>
-            <section className="icon-list">
-                <i className="nes-mario"></i>
-                <i className="nes-ash"></i>
-                <i className="nes-pokeball"></i>
-                <i className="nes-bulbasaur"></i>
-                <i className="nes-charmander"></i>
-                <i className="nes-squirtle"></i>
-                <i className="nes-kirby"></i>
-                <i className="nes-octocat animate"></i>
-            </section>
+            <br></br>
+
+            <p class="nes-balloon from-left nes-pointer">要去吃好吃的~Friday night!</p>
+
+            <br></br>
+            <i className="nes-kirby"></i>
+            <i className="nes-octocat animate"></i>
         </div>
     )
 }
